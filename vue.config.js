f2f14612082e225d8a346cbd90ca9048d6e43114
vinson <cao.wenjun@htnova.com>
2019-09-25 15:21:12 +0800

    修改转发端口

--- vue.config.js
+++ vue.config.js
@@ -41,7 +41,7 @@ module.exports = {
       // detail: https://cli.vuejs.org/config/#devserver-proxy
       [process.env.VUE_APP_BASE_API]: {
         // target: `http://127.0.0.1:${port}/mock`,
-        target: `http://127.0.0.1:1968/`,
+        target: `http://127.0.0.1:19687/`,
         changeOrigin: true
         // pathRewrite: {
         //   ['^' + process.env.VUE_APP_BASE_API]: ''
