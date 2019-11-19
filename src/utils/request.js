8ff0c6c4bdf2bf6e532bf2d37b66f0c026e887d7
vinson <cao.wenjun@htnova.com>
2019-11-19 10:25:58 +0800

    axios错误消息提示修改

--- src/utils/request.js
+++ src/utils/request.js
@@ -77,32 +77,37 @@ service.interceptors.response.use(
     formatMsg(res)
     console.log('err' + error) // for debug
 
-    // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-    if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+    let msg
+    if (error.response.status === 404) {
+      msg = '接口不存在'
+    } else if (error.response.status === 403) {
+      msg = '无权访问'
+    } else if (error.response.status === 401) {
+      msg = '认证失败，请重新登录'
+    }
+    Message({
+      message: res.msg || msg || '服务器错误',
+      type: 'error',
+      duration: 5 * 1000
+    })
+
+    if (error.response.status === 401) {
       // to re-login
-      MessageBox.confirm('当前登录已过期，请重新登录', '提示', {
-        confirmButtonText: '重新登录',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
+      if (error.config.url.startWith(process.env.VUE_APP_BASE_API + '/auth/info')) {
         store.dispatch('user/resetToken').then(() => {
           location.reload()
         })
-      })
-    } else {
-      let msg
-      if (error.response.status === 404) {
-        msg = '接口不存在'
-      } else if (error.response.status === 403) {
-        msg = '无权访问'
-      } else if (error.response.status === 401) {
-        msg = '认证失败，请重新登录'
+      } else {
+        MessageBox.confirm('当前登录已过期，请重新登录', '提示', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
       }
-      Message({
-        message: res.msg || msg || '服务器错误',
-        type: 'error',
-        duration: 5 * 1000
-      })
     }
 
     return Promise.reject(error)
