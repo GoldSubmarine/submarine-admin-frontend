9a3572550dc43ac2e0ad8931bc5d5f1348bcefad
GoldSubmarine <997462392@qq.com>
2020-07-02 12:03:19 +0800

    icon添加

--- src/router/modules/systemWorkflow.js
+++ src/router/modules/systemWorkflow.js
@@ -7,7 +7,7 @@ const systemWorkflowRouter = {
   name: 'Workflow',
   meta: {
     title: '工作流程',
-    icon: 'activiti',
+    icon: 'workflow',
     permission: ['workflow']
   },
   children: [
@@ -15,37 +15,37 @@ const systemWorkflowRouter = {
       path: 'model',
       component: () => import('@/views/system/workflow/model/index'),
       name: 'WorkflowModel',
-      meta: { title: '模型管理', icon: 'activiti', permission: ['actModel'] }
+      meta: { title: '模型管理', icon: 'workflowModel', permission: ['actModel'] }
     },
     {
       path: 'process',
       component: () => import('@/views/system/workflow/process/index'),
       name: 'WorkflowProcess',
-      meta: { title: '流程管理', icon: 'activiti', permission: ['actProcess'] }
+      meta: { title: '流程管理', icon: 'workflowProcess', permission: ['actProcess'] }
     },
     {
       path: 'launch',
       component: () => import('@/views/system/workflow/launch/index'),
       name: 'WorkflowLaunch',
-      meta: { title: '发起申请', icon: 'activiti', permission: ['actProcess'] }
+      meta: { title: '发起申请', icon: 'workflowLaunch', permission: ['actProcess'] }
     },
     {
       path: 'apply',
       component: () => import('@/views/system/workflow/apply/index'),
       name: 'WorkflowApply',
-      meta: { title: '我的申请', icon: 'activiti', permission: ['actProcess'] }
+      meta: { title: '我的申请', icon: 'workflowApply', permission: ['actProcess'] }
     },
     {
       path: 'todo',
       component: () => import('@/views/system/workflow/todo/index'),
       name: 'WorkflowTodo',
-      meta: { title: '我的待办', icon: 'activiti', permission: ['actProcess'] }
+      meta: { title: '我的待办', icon: 'workflowTodo', permission: ['actProcess'] }
     },
     {
       path: 'done',
       component: () => import('@/views/system/workflow/done/index'),
       name: 'WorkflowDone',
-      meta: { title: '我的已办', icon: 'activiti', permission: ['actProcess'] }
+      meta: { title: '我的已办', icon: 'workflowDone', permission: ['actProcess'] }
     }
   ]
 }
